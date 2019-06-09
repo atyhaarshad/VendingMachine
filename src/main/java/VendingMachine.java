8bb370e9345917ddb2d0b2f2671a97133cde5f43
atyha <atyhaarshad1@hotmail.com>
2019-06-09 18:55:02 +0100

    vending machine add coin test passed

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 
-public class VendingMachine { // 187
+public class VendingMachine {
 
     private ArrayList<Drawer> drawers;
     private ArrayList<Coin> coinsEntered;
     private CoinReturn coinReturn;
+    private double credit;
 
-    public VendingMachine(CoinReturn coinReturn) {
+    public VendingMachine(CoinReturn coinReturn, double credit) {
         this.coinReturn = coinReturn;
         this.coinsEntered = new ArrayList<Coin>();
         this.drawers = new ArrayList<Drawer>();
+        this.credit = 0;
     }
 
 
@@ -18,12 +20,24 @@ public class VendingMachine { // 187
     }
 
 
-
-    public boolean isCoinValid(Coin coin) {
-        if (coin.getCoinTypeValue() < 0.05){
+    public boolean validCoin(Coin coin) {
+        if (coin.getCoinTypeValue() < CoinType.FIVE.getValue()){
             return false;
         } else {
             return true;
         }
+    }
+
+
+    public void addCoin(Coin coin) {
+        if (validCoin(coin) == true){
+            coinsEntered.add(coin);
+        } else {
+            coinReturn.addCoin(coin);
+        }
+    }
+
+    public int getNoOfCoinsEntered(){
+        return coinsEntered.size();
     }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,11 +1,15 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class VendingMachineTest {
 
     VendingMachine vendingMachine;
+    private ArrayList<Coin> coinsEntered;
+    private ArrayList<Drawer> drawers;
     Coin pound;
     Coin fiftyP;
     Coin twentyP;
@@ -15,9 +19,10 @@ public class VendingMachineTest {
     Coin twoP;
     CoinReturn coinReturn;
 
+
     @Before
     public void setUp(){
-        vendingMachine = new VendingMachine(coinReturn);
+        vendingMachine = new VendingMachine(coinReturn, 0);
         pound = new Coin(CoinType.POUND);
         fiftyP = new Coin(CoinType.FIFTY);
         twentyP = new Coin(CoinType.TWENTY);
@@ -26,17 +31,20 @@ public class VendingMachineTest {
         oneP = new Coin(CoinType.ONE);
         twoP = new Coin(CoinType.TWO);
         coinReturn = new CoinReturn();
+        coinsEntered = new ArrayList<Coin>();
+        drawers = new ArrayList<Drawer>();
 
     }
 
     @Test
     public void canAddCoinToMachine(){
-        coinReturn.addCoin(pound);
-        assertEquals(1.00, vendingMachine.getTotalAmount(coinReturn));
+        vendingMachine.addCoin(pound);
+        assertEquals(1, vendingMachine.getNoOfCoinsEntered());
     }
 
     @Test
     public void canCheckCoinValid(){
-        assertEquals(false, vendingMachine.isCoinValid(twoP));
+        assertEquals(false, vendingMachine.validCoin(twoP));
     }
+
 }
